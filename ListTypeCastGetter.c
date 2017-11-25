d0681b0f98bed1280faf310f23b258670fd12895
rxiangle <33599891+rxiangle@users.noreply.github.com>
2017-11-25 11:55:08 +0700

    Add files via upload
    completing unit getter and setter

--- ListTypeCastGetter.c
+++ ListTypeCastGetter.c
@@ -63,3 +63,121 @@ int setBuildingType (Building building, char NewType)
     building.Type = NewType;
     return &building;
 }
+
+int getUnitMaxHealth (int addr)
+// Usage : int = getUnitMaxHealth (Info(P))
+{
+    return getUnit(addr).MaxHealth;
+}
+
+int getUnitHealth (int addr)
+// Usage : int = getUnitHealth (Info(P))
+{
+    return getUnit(addr).Health;
+}
+
+int getUnitAttack (int addr)
+// Usage : int = getUnitAttack (Info(P))
+{
+    return getUnit(addr).Attack;
+}
+
+int getUnitMaxMove (int addr)
+// Usage : int = getUnitMaxMove (Info(P))
+{
+    return getUnit(addr).MaxMove;
+}
+
+int getUnitMove (int addr)
+// Usage : int = getUnitMove (Info(P))
+{
+    return getUnit(addr).Move;
+}
+
+char getUnitAttackType(int addr)
+// Usage : char = getUnitAttackType (Info(P))
+{
+    return getUnit(addr).AttackType;
+}
+
+boolean getUnitHit(int addr)
+// Usage : boolean = getUnitHit (Info(P))
+{
+    return getUnit(addr).Hit;
+}
+
+POINT getUnitLocation(int addr)
+// Usage : location = getUnitLocation (Info(P))
+{
+    return getUnit(addr).Location;
+}
+
+int getUnitHarga (int addr)
+// Usage : harga = getUnitHarga (Info(P))
+{
+    return getUnit(addr).Harga;
+}
+
+
+int setUnitMaxHealth (TypeUnit unit, int MaxHealth)
+// Usage : Info(P) = setUnitMaxHealth(MaxHealth)
+{
+    unit.MaxHealth = MaxHealth;
+    return &unit;
+}
+
+int setUnitHealth (TypeUnit unit, int Health)
+// Usage : Info(P) = setUnitHealth(Health)
+{
+    unit.Health = Health;
+    return &unit;
+}
+
+int setUnitAttack (TypeUnit unit, int Attack)
+// Usage : Info(P) = setUnitAttack(Attack)
+{
+    unit.Attack = Attack;
+    return &unit;
+}
+
+int setUnitMaxMove (TypeUnit unit, int MaxMove)
+// Usage : Info(P) = setUnitMaxMove(MaxMove)
+{
+    unit.MaxMove = MaxMove;
+    return &unit;
+}
+
+int setUnitMove (TypeUnit unit, int Move)
+// Usage : Info(P) = setUnitMove(Move)
+{
+    unit.Move = Move;
+    return &unit;
+}
+
+char setUnitAttackType(TypeUnit unit, char AttackType)
+// Usage : Info(P) = setUnitAttackType(AttackType)
+{
+    unit.AttackType = AttackType;
+    return &unit;
+}
+
+boolean setUnitHit(TypeUnit unit, boolean Hit)
+// Usage : Info(P) = setUnitHit(Hit)
+{
+    unit.Hit = Hit;
+    return &unit;
+}
+
+POINT setUnitLocation(TypeUnit unit, POINT Location)
+// Usage : Info(P) = setUnitLocation(Location)
+{
+    unit.Location = Location;
+    return &unit;
+}
+
+int setUnitHarga (TypeUnit unit, int Harga)
+// Usage : Info(P) = setUnitHarga(Harga)
+{
+    unit.Harga = Harga;
+    return &unit;
+}
--- ListTypeCastGetter.h
+++ ListTypeCastGetter.h
@@ -28,9 +28,61 @@ int setBuildingGold (Building building, int Gold);
 int setBuildingType (Building building, char NewType);
 // Usage : Info(P) = setBuildingType(NewType)
 
-TypeUnit getUnit (int addr);
-// Usage : unit = getUnit (Info(P))
 
 
+int getUnitMaxHealth (int addr);
+// Usage : int = getUnitMaxHealth (Info(P))
+
+int getUnitHealth (int addr);
+// Usage : int = getUnitHealth (Info(P))
+
+int getUnitAttack (int addr);
+// Usage : int = getUnitAttack (Info(P))
+
+int getUnitMaxMove (int addr);
+// Usage : int = getUnitMaxMove (Info(P))
+
+int getUnitMove (int addr);
+// Usage : int = getUnitMove (Info(P))
+
+char getUnitAttackType(int addr);
+// Usage : char = getUnitAttackType (Info(P))
+
+boolean getUnitHit(int addr);
+// Usage : boolean = getUnitHit (Info(P))
+
+POINT getUnitLocation(int addr);
+// Usage : location = getUnitLocation (Info(P))
+
+int getUnitHarga (int addr);
+// Usage : harga = getUnitHarga (Info(P))
+
+
+int setUnitMaxHealth (TypeUnit unit, int MaxHealth);
+// Usage : Info(P) = setUnitMaxHealth(MaxHealth)
+
+int setUnitHealth (TypeUnit unit, int Health);
+// Usage : Info(P) = setUnitHealth(Health)
+
+int setUnitAttack (TypeUnit unit, int Attack);
+// Usage : Info(P) = setUnitAttack(Attack)
+
+int setUnitMaxMove (TypeUnit unit, int MaxMove);
+// Usage : Info(P) = setUnitMaxMove(MaxMove)
+
+int setUnitMove (TypeUnit unit, int Move);
+// Usage : Info(P) = setUnitMove(Move)
+
+char setUnitAttackType(TypeUnit unit, char AttackType);
+// Usage : Info(P) = setUnitAttackType(AttackType)
+
+boolean setUnitHit(TypeUnit unit, boolean Hit);
+// Usage : Info(P) = setUnitHit(Hit)
+
+POINT setUnitLocation(TypeUnit unit, POINT Location);
+// Usage : Info(P) = setUnitLocation(Location)
+
+int setUnitHarga (TypeUnit unit, int Harga);
+// Usage : Info(P) = setUnitHarga(Harga)
 
 #endif
