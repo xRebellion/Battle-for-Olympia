612fd64e33ccb991cc146c4ac6c043c97bbbaa59
cksputra <33357278+cksputra@users.noreply.github.com>
2017-11-27 20:13:25 +0700

    Update turn.c

--- turn.c
+++ turn.c
@@ -7,4 +7,26 @@ void ChangeTurn (Queue *P) {
     DelQ(&(*P), &temp);
     AddQ(&(*P), temp);
     UpdateInfo(&InfoHead(*P));
+    RefreshMovementAndAttack(&InfoHead(*P));
+
+}
+
+void RefreshMovementAndAttack (Player *P){
+//Mengisi kembali movement unit dengan max move dan attack condition
+    //Kamus lokal
+    addressU X;
+    TypeUnit Unit;
+    //Algoritma
+    if (!IsEmptyUnit((*P).Unit)){
+        X = First((*P).Unit);
+        Unit = Info(X);
+        Unit.Move = Unit.MaxMove;
+        Unit.Attack = true;
+        while (Next(X) != Nil) {
+            X = Next(X);
+            Unit = Info(X);
+            Unit.Move = Unit.MaxMove;
+            Unit.Attack = true;
+        }
+    }
 }
