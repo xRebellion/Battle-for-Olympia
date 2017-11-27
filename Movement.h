f69995888f85265fa61e49188ba3b7a2aac06cfc
rxiangle <33599891+rxiangle@users.noreply.github.com>
2017-11-27 20:44:05 +0700

    added stack undo

--- Movement.h
+++ Movement.h
@@ -2,7 +2,7 @@
 
 #include "init.h"
 #include "Map.h"
-
+#include <math.h>
 
 
 char determineDirection(TypeUnit unit, int x, int y);
@@ -18,6 +18,12 @@ boolean isThereUnit (Player P1, TypeUnit unit, int x, int y);
 
 boolean isMovePossible (Player P1, Player P2, Map M, TypeUnit unit, char direction, int x, int y);
 
+void PushStack (Stack *S, Player P1, Map M, TypeUnit currUnit);
+
+void Undo (Stack *S, Player *P1, Map *M, TypeUnit *currUnit);
+
 boolean Move(Player P1, Player P2, Map * M, TypeUnit unit, int x, int y);
 
 void InsertDummyMap(Player P1, Player P2, Map M, Map * MDummy, TypeUnit currUnit);
+
+void MoveCommand(Stack *S, Player P1, Player P2, Map * M, TypeUnit *currUnit, POINT * PrevLoc);
